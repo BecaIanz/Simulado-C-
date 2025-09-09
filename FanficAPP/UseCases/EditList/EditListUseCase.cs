37576971fd84cc518536459b9ae4a58fdf73f5b4
BecaIanz <silveiraianzr@gmail.com>
2025-09-09 11:44:01 -0300

    Implemented EditListUseCase

--- FanficAPP/UseCases/EditList/EditListUseCase.cs
+++ FanficAPP/UseCases/EditList/EditListUseCase.cs
@@ -32,7 +32,9 @@ public class EditListUseCase(
         };
 
         ctx.ReadingLists.Add(fanficList);
-        
+
+        list.LastModificationDate = DateTime.UtcNow;
+
         return Result<EditListResponse>.Sucess(null);
     }
 }
